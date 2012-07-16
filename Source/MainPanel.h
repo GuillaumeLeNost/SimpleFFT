8988ea5bd9616e072b39f7c8d303da764fc79b1c
tom webster <infoserv@Infoserves-MacBook-2.local>
2012-07-16 16:08:32 +0100

    header

--- Source/MainPanel.h
+++ Source/MainPanel.h
@@ -23,7 +23,7 @@
 #define __JUCER_HEADER_MAINPANEL_MAINPANEL_CEFA4231__
 
 //[Headers]     -- You can add your own extra header files here --
-#include "juce.h"
+#include "../JuceLibraryCode/JuceHeader.h"
 //[/Headers]
 
 
