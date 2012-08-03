97cc0593a51dff7dbddf1fe06674ce1d4a24bc3c
mars <martin@rebel-it.com>
2012-08-03 14:09:26 +0100

    added FFTW3 wrapper class and dependencies

---
+++ Source/FastFourierTransformer.h
@@ -0,0 +1,21 @@
+/*
+  ==============================================================================
+
+    FastFourierTransformer.h
+    Created: 3 Aug 2012 8:13:37am
+    Author:  Martin Klang
+
+  ==============================================================================
+*/
+
+#ifndef __FASTFOURIERTRANSFORMER_H_9F2E8A4F__
+#define __FASTFOURIERTRANSFORMER_H_9F2E8A4F__
+
+class FastFourierTransformer {
+public:
+	
+};
+
+
+
+#endif  // __FASTFOURIERTRANSFORMER_H_9F2E8A4F__
